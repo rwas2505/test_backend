d421939dbd2d429eb06a6c092bb3940305895c2d
ryan <ryanwaszak@gmail.com>
2020-05-05 20:22:37 -0500

    add student create to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+student = Student.create!(
+  first_name: "John", 
+  last_name: "Smith", 
+  email: "john@email.com(opens in new tab)", 
+  phone_number: "555-555-5555", 
+  short_bio: "I go to work. I come back from work. I watch the M...", 
+  linkedin_url: "linkedin URL", 
+  twitter_handle: "twitter handle", 
+  personal_site_url: "personal site URL", 
+  online_resume_url: "online resume URL", 
+  github_url: "github URL", 
+  photo_url: "photo URL", 
+  password_digest: "password"
+)
